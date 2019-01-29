4cfd6f54f4f1c54d9707d5ac4bbfbc5331bca23d
sfattal <fattal.sherif@gmail.com>
2019-01-28 19:53:03 -0500

    fix signup/signin routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -23,21 +23,21 @@ module.exports = function(app) {
   });
 
   // Load create post page
-  app.get("/home/new-post", function(req, res) {
-    res.render("post", {
-      examples: dbExamples
-    });
-  });
-
-  // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function() {
-  //     dbExample;
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
+  // app.get("/home/new-post", function(req, res) {
+  //   res.render("post", {
+  //     examples: dbExamples
   //   });
   // });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function() {
+      dbExample;
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
